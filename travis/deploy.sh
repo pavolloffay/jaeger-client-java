af97e9c0cae1f5bc3e861b47126ce44ff830c362
Pavol Loffay <ploffay@redhat.com>
2019-05-10 12:23:08 +0200

    Add github secrets
    Signed-off-by: Pavol Loffay <ploffay@redhat.com>

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -3,10 +3,16 @@
 set -o errexit -o nounset -o pipefail
 
 
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
 if [[ "$TRAVIS_TAG" =~ ^release-[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+?$ ]]; then
     echo "We are on release- tag"
     echo "bumping versions and creating vx.x.x tag"
     echo "final artifact will be published in build for the tag"
+    setup_git
     version=$(echo "${TRAVIS_TAG}" | sed 's/^release-//')
     ./gradlew release release -Prelease.useAutomaticVersion=true -Prelease.releaseVersion=${version}
 else
